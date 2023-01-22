a1477ad2b631bd1c4671ff7f1d866c8792752668
robiuzzaman4 <robiuzzaman4@gmail.com>
2023-01-22 16:25:48 +0600

    done

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ module.exports = {
       colors: {
         "white-1": "#ffffff",
         "dark-1": "#1C1C1C",
+        // "dark-2": "#494949",
         "dark-3": "#777777",
+        "dark-5": "#D2D2D2",
         "dark-7": "#F4F4F4",
         "fresh-lemon": "rgba(87, 118, 57, 0.1)",
         "fresh-orange": "rgba(230, 166, 35, 0.1)"
