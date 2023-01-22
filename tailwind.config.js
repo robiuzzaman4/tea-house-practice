7e95fa4ec5a644b48557fa689a0fea0e583caada
robiuzzaman4 <robiuzzaman4@gmail.com>
2023-01-22 08:20:00 +0600

    test-1

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "manrope": ["Manrope", "sans-serif"]
+      },
+      colors: {
+        "white-1": "#ffffff",
+        "dark-1": "#1C1C1C",
+        "dark-3": "#777777",
+        "dark-7": "#F4F4F4",
+        "linear-gradient": "linear-gradient(to left, #FF0000, #FF8938)"
+      }
+    },
+  },
+  plugins: [],
+}
